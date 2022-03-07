cf93ffc53045e1050313579df14d83d8f9a74c5d
AvinashPJ <59468133+AvinashPJ-dotnet@users.noreply.github.com>
2022-03-07 13:34:51 +0530

    changed: in remote repo

--- src/main/MyTestClass.java
+++ src/main/MyTestClass.java
@@ -4,5 +4,6 @@ public class MyTestClass {
     public MyTestClass(){
     System.out.printf("hey this is my test class edited in remote");
     System.out.printf("hey this is my test class edited in local branch");
+    System.out.printf("hey this is my test class edited in remote second time");
 }
 }
