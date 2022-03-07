4c8286a81a325f59cf0f0530120aa8a9d086ab4a
AvinashPJ <59468133+AvinashPJ-dotnet@users.noreply.github.com>
2022-03-07 12:50:50 +0530

    chenged: in remote textclass file

--- src/main/MyTestClass.java
+++ src/main/MyTestClass.java
@@ -2,6 +2,6 @@ package main;
 
 public class MyTestClass {
     public MyTestClass(){
-    System.out.printf("hey this is my test class hello");
+    System.out.printf("hey this is my test class edited in remote");
 }
 }
