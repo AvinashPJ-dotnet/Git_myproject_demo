2a40e7e07f1c5e9c3924581265d07cf52f84cae0
AvinashpIntelora <avinash.p@intelora.co.in>
2022-03-07 12:27:43 +0530

    changed: in mytestclass print state ment

--- src/main/MyTestClass.java
+++ src/main/MyTestClass.java
@@ -1,13 +1,7 @@
 package main;
 
 public class MyTestClass {
-<<<<<<< HEAD
     public MyTestClass(){
-        System.out.printf("hey this is my test class");
-    }
-=======
-public MyTestClass(){
-    System.out.printf("hey this is my test class Edited in remote");
+    System.out.printf("hey this is my test class");
 }
->>>>>>> daeddb39f2953ac7629f99be238085c04310c5aa
 }
