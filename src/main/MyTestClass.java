6ad8fc58fe426531718fa3f3ff97fbeef6901c69
AvinashpIntelora <avinash.p@intelora.co.in>
2022-03-07 13:21:10 +0530

    changed: in brach testclass file

--- src/main/MyTestClass.java
+++ src/main/MyTestClass.java
@@ -3,5 +3,6 @@ package main;
 public class MyTestClass {
     public MyTestClass(){
     System.out.printf("hey this is my test class edited in remote");
+    System.out.printf("hey this is my test class edited in local branch");
 }
 }
